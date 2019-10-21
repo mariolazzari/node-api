f340bea531025028944dd4d504d76f561a46c6e0
Mario Lazzari <mario.lazzari@italcoeng.com>
2019-10-21 11:05:53 +0200

    Done chapter 3

---
+++ nodehttp/server.js
@@ -0,0 +1,23 @@
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  //console.log(req.method);
+  res.statusCode = 200;
+  res.setHeader("X-Powerd-By", "NodeJS");
+  res.setHeader("Content-Type", "application/json");
+  const todos = [
+    { id: 1, text: "test 1" },
+    { id: 2, text: "test 2" },
+    { id: 3, text: "test 3" }
+  ];
+  //res.write("<h1>Ciao</h1>");
+  res.end(
+    JSON.stringify({
+      success: true,
+      data: todos
+    })
+  );
+});
+
+const PORT = 5000;
+server.listen(PORT, () => console.log(`Server started on port ${PORT}`));
