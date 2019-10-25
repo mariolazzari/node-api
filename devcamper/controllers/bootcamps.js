a3076e204b2147c846491a744198df7ca6ba8dd7
Mario Lazzari <mario.lazzari@italcoeng.com>
2019-10-25 17:28:06 +0200

    Done lesson 1 chapter 8

--- devcamper/controllers/bootcamps.js
+++ devcamper/controllers/bootcamps.js
@@ -21,6 +21,20 @@ exports.getBootcamp = asyncHandler(async (req, res, next) => {
 
 // add new bootcamp
 exports.createBootcamp = asyncHandler(async (req, res, next) => {
+  // add user to req body
+  req.body.user = req.user;
+  // check for published bootcamp
+  const publishedBootcamp = await Bootcamp.findOne({ user: req.user.id });
+  // if user is not an admin, one bootcamp only allowed
+  if (publishedBootcamp && req.user.role !== "admin") {
+    return next(
+      new ErrorResponse(
+        `The user with ID ${req.user.id} has already published a bootcamp.`
+      )
+    );
+  }
+
+  // save new bootcamp
   const bootcamp = await Bootcamp.create(req.body);
   res.status(201).json({ success: true, data: bootcamp });
 });
--- devcamper/seeder.js
+++ devcamper/seeder.js
@@ -5,6 +5,7 @@ const connectDB = require("./config/db");
 // load models
 const Bootcamp = require("./models/Bootcamp");
 const Course = require("./models/Course");
+const User = require("./models/User");
 
 // database connection
 connectDB();
@@ -16,12 +17,16 @@ const bootcamps = JSON.parse(
 const courses = JSON.parse(
   fs.readFileSync(`${__dirname}/_data/courses.json`, "utf-8")
 );
+const users = JSON.parse(
+  fs.readFileSync(`${__dirname}/_data/users.json`, "utf-8")
+);
 
 // import data into mongo db
 const importData = async () => {
   try {
     await Bootcamp.create(bootcamps);
     await Course.create(courses);
+    await User.create(users);
     console.log("Data imported...".green.inverse);
     process.exit();
   } catch (error) {
@@ -34,6 +39,7 @@ const deleteData = async () => {
   try {
     await Bootcamp.deleteMany();
     await Course.deleteMany();
+    await User.deleteMany();
     console.log("Data deleted...".red.inverse);
     process.exit();
   } catch (error) {
